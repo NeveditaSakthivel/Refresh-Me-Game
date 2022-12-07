10d195ac11fc2568138271f954c6b70f67318474
NeveditaSakthivel <120033497+NeveditaSakthivel@users.noreply.github.com>
2022-12-07 21:08:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var randomnumber = Math.floor(Math.random() * 6 ) + 1 ;
 var take = document.querySelectorAll("img")[0];
-var change = "Images/dice" + randomnumber + ".png";
+var change = "dice" + randomnumber + ".png";
 take.setAttribute("src",change);
 
 var randomnumber2 = Math.floor(Math.random() * 6 ) + 1 ;
 var take2 = document.querySelectorAll("img")[1];
-var change2 = "Images/dice" + randomnumber2 + ".png";
+var change2 = "dice" + randomnumber2 + ".png";
 take2.setAttribute("src",change2);
 
 if(randomnumber > randomnumber2)
